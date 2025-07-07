c7404e2affdfc35e41e0b4a65c788c2b687fe51e
DamianoMura <mura.damiano14@gmail.com>
2025-07-07 18:19:06 +0200

    [add] palindroma

---
+++ script.js
@@ -0,0 +1,53 @@
+// Palidroma
+
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// Pari e Dispari
+
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+// Consigli del giorno
+
+// Scriviamo sempre in italiano i passaggi che vogliamo fare
+// Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+
+// Domande da farsi quando si crea una funzione:
+// Come dovrebbe chiamarsi?
+// Ho bisogno di parametri?
+// Devo restituire un valore?
+// Se sì, di che tipo?
+
+
+//Palindroma 
+
+const word=prompt("inserisci una parola e vediamo se è palindroma")
+
+//inizializziamo la funzione e diamo word come parametro
+
+function palindrome(word){
+ // rovesciamo la parola con un for 
+  //dichiariamo una stringa di supporto
+  let reversed;  
+  for(let i=word.length ; i>=0; i--){
+    if(reversed==undefined){
+      reversed=word[i];
+    }
+    else{
+      reversed+=word[i];
+    }
+  }
+  if(word===reversed) return true;
+  else return false;
+}
+
+if(palindrome(word)==true){
+  console.log("è palindroma")
+}
+else{
+  console.log("non palindroma")
+}
